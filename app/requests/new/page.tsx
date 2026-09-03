3ef79515807c82f13468157bb0aa058f2629a0b6
husban <husban@Mac-3820.lan>
2026-09-03 09:20:20 -0700

    Create request submission form

---
+++ app/requests/new/page.tsx
@@ -0,0 +1,11 @@
+export default function NewRequestPage() {
+    return(
+    <div className="mx-auto max-w-2xl p-6">
+        <h1 className="text-2xl font-bold text-left">
+         Create a New Request
+        </h1>
+        <p>Submit a request for review</p>
+        
+    </div>
+ )
+}
