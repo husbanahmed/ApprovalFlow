550e2fbeb70e68e15240c41aa9e15e1191efe65b
husban <husban@Mac.lan>
2026-09-05 17:19:46 -0700

    Add request title field

--- app/requests/new/page.tsx
+++ app/requests/new/page.tsx
@@ -1,11 +1,20 @@
 export default function NewRequestPage() {
     return(
-    <div className="mx-auto max-w-2xl p-6">
-        <h1 className="text-2xl font-bold text-left">
+    <div className="max-w-2xl p-6 text-center">
+        <h1 className="text-2xl font-bold text-center mt-2">
          Create a New Request
         </h1>
         <p>Submit a request for review</p>
-        
+
+        <div className="flex flex-col gap-4">
+            <label className="text-left m-2" >
+                Request Title
+            </label>
+            <input 
+            className="border rounded-md px-3"
+            placeholder=" Type Request here " />
+        </div>
+       
     </div>
  )
 }
