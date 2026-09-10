f64148bea2e4718267d831ae332980a8d0479341
husban <husban@Mac.lan>
2026-09-09 23:46:20 -0700

    Handle request form submission

--- app/requests/new/page.tsx
+++ app/requests/new/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
 export default function NewRequestPage() {
+
+    function handleSubmit(event) {
+
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget)
+
+        console.log(formData.get("title"))
+        console.log(formData.get("description"))
+        console.log(formData.get("priority"))
+
+    }
+
     return(
-    <div className="max-w-auto p-6 text-center">
+    <form onSubmit={handleSubmit} className="max-w-auto p-6 text-center">
         <h1 className="text-2xl font-bold text-center mt-2">
          Create a New Request
         </h1>
@@ -11,18 +26,19 @@ export default function NewRequestPage() {
                 Request Title
             </label>
             <input 
+            name="title"
             className="border rounded-md px-3"
             placeholder=" Type Request here " />
         </div>
 
         <div>
             <p className="text-left m-4">Description</p>
-            <textarea className="border w-full rounded-md px-3"></textarea>
+            <textarea name="description" className="border w-full rounded-md px-3"></textarea>
         </div>
 
         <div className="text-left m-4">
             <p>Priority</p>
-            <select className="border" >
+            <select name= "priority" className="border" >
                 <option value=""hidden> Please choose an option</option>
                 <option>Low</option>
                 <option>Medium</option>
@@ -31,10 +47,10 @@ export default function NewRequestPage() {
         </div>
 
         <div className="flex flex-row justify-center gap-4 mt-6">
-            <button className="rounded-md border py-4 px-4 bg-red-600 hover:bg-red-300 text-white"> Cancel </button>
-            <button className="rounded-md border py-4 px-4 bg-blue-600 hover:bg-blue-300 text-white"> Submit </button>
+            <button type="button" className="rounded-md border py-4 px-4 bg-red-600 hover:bg-red-300 text-white"> Cancel </button>
+            <button type="submit" className="rounded-md border py-4 px-4 bg-blue-600 hover:bg-blue-300 text-white"> Submit </button>
         </div>
        
-    </div>
+    </form>
  )
 }
